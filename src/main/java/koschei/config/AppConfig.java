1400d97e21593fda9e2a495078e89107ed229432
Denis Bikkuzin <bikkuzin.den@gmail.com>
2024-05-11 18:19:28 +0500

    2.1.4 task!

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
+    private Egg6 egg6;
+
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+
 }
